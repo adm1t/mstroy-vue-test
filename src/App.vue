7c5a201d88f16ac8e9f6236b65d481903141c4a5
Дмитрий Филиппов <filippov95@mail.ru>
2026-08-10 18:27:49 +0300

    tech: инициализация репозитория, стартовый темплейт

---
+++ src/App.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Earum fuga harum quisquam neque tenetur cum repellat exercitationem repellendus molestias delectus.</h1>
+</template>
